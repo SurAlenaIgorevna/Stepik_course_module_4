7b2ed308a55d8d72a361db67e913810270814945
Alena Sidorova <sur.alena.igorevna@gmail.com>
2019-08-01 00:36:47 +0300

    Login from product page

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators(object):
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
 
 class LoginPageLocator(object):
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -12,3 +13,8 @@ class ProductPageLocators(object):
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success')
+
+class BasePageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -10,3 +10,12 @@ class ProductPage(BasePage):
     def check_success_message(self):
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text == self.browser.find_elements_by_css_selector('.alertinner > strong')[0].text
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text == self.browser.find_element_by_css_selector('.alertinner > p > strong').text
+
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message doesn't disappear"
