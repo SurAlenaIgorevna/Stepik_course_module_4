1425ed8c01d88bf52d3d99af926af4d67a642844
Alena Sidorova <sur.alena.igorevna@gmail.com>
2020-01-23 16:35:07 +0000

    basket_page added (fix)

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,9 +3,3 @@ from .base_page import BasePage
 
 class BasketPage(BasePage):
     pass
-     #def guest_cant_see_product_in_basket_opened_from_main_page(self):
-
-      #  assert self.is_not_element_present(*BasePageLocators.PRODUCT_ADDED_TO_BASKET), \
-      #      "Product is presented, but should not be"
-       # empty_basket = self.browser.find_element(*BasePageLocators.EMPTY_BASKET).text
-        #assert "Your basket is empty" in empty_basket, "Your basket is not empty"
